83b981d9b8871c1326825862dce0ebcba94c9260
Tomasz Dyrda <tomasz.dyrda@100sw.pl>
2022-04-12 20:46:53 +0200

    Rest of user functions

--- Project2/src/Controllers/userController.ts
+++ Project2/src/Controllers/userController.ts
@@ -59,7 +59,37 @@ router.post('/login', async (req: Request, res: Response) => {
     }
 
 })
+router.get('/logout', auth, async (req:Request, res:Response) => {
+    res
+      .clearCookie("accesstoken")
+      .status(200)
+      .send("Wylogowałeś się")  
+    });
 
+router.get('/testget', auth, async (req:Request, res:Response) => {
+    let x = req.headers.userId
+    let y = req.headers.username
+    res.status(200).send(`${x} ${y}`)
+    });
+
+router.get('/protected', async (req:Request, res:Response) => {
+    let x = req.cookies
+    res.status(200).send(x)
+    })
+
+router.delete('/delete',auth, async (req: Request, res: Response) => {
+
+    try{
+        let x = req.headers.userId
+        let deletedUser = await _userService.DeleteUser(x)
+        res.clearCookie("accesstoken").status(200).send(`ID: ${deletedUser.id}`)
+    }
+    catch(error)
+    {
+        res.status(500).send(error);
+    }
+
+})
 
 
 module.exports = router;
