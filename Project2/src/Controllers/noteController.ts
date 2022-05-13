1797084d17917ac144279b72516d3fdba8f449fc
Tomasz Dyrda <tomasz.dyrda@100sw.pl>
2022-05-13 23:12:50 +0200

    impelemnt in noteController edit, delete, get etc.

--- Project2/src/Controllers/noteController.ts
+++ Project2/src/Controllers/noteController.ts
@@ -27,7 +27,69 @@ router.post('/add',auth, async (req: Request, res: Response) => {
     }
 })
 
+router.put('/edit/:id', async (req: Request, res: Response) => {
 
+    res.status(200).send("success");
+})
+
+
+router.delete('/delete/:id', auth, async (req: Request, res: Response) => {
+    
+    try
+    {
+        const id:any = req.params.id
+
+        let note = await noteService.GetNoteById(id)
+        if(!note)
+        {
+            res.status(400).send("Note doesn`t exists.");
+        }
+        const userIdNote = note.UserId
+        const userIdToken = req.headers.userId
+        if(userIdNote == userIdToken)
+        {
+            noteService.DeleteNote(id)
+            res.status(200).send("Note has been deleted.");
+        }
+        else
+        {
+            res.status(400).send("Your are not the owner");
+        }
+    }
+    catch(e)
+    {
+        
+    }
+})
+router.get('/getall', async (req: Request, res: Response) => {
+
+    let notes = await noteService.GetNotes()
+    res.status(200).send(notes);
+})
+
+router.get('/get/:id', async (req: Request, res: Response) => {
+
+    let note = await noteService.GetNoteById(req.params.id)
+    res.status(200).send(note);
+})
+
+
+router.get('/tag/:tag', async (req: Request, res: Response) => {
+
+    const tag:any = req.params.tag
+    let notes = await noteService.GetByTag(tag)
+    
+    res.status(200).send(notes);
+})
+
+router.get('/user/:id', auth, async (req: Request, res: Response) => {
+
+    const userId = req.headers.userId
+
+    const id:any = req.params.id
+    let notes = await noteService.GetByUserId(id, userId)
+    res.status(200).send(notes);
+})
 
 
 module.exports = router;
